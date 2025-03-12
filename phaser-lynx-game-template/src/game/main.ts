60debd5a28ba28b463167e1a9afaa82a40eb4299
Patrick Klly <doitformurphy@gmail.com>
2025-03-12 17:01:04 -0400

    Add PacmanGame scene and update game configuration

--- phaser-lynx-game-template/src/game/main.ts
+++ phaser-lynx-game-template/src/game/main.ts
@@ -3,6 +3,7 @@ import { Boot } from './scenes/Boot';
 import { Game as MainGame } from './scenes/Game';
 import { GameOver } from './scenes/GameOver';
 import { MainMenu } from './scenes/MainMenu';
+import { PacmanGame } from './scenes/PacmanGame';
 import { Preloader } from './scenes/Preloader';
 
 //  Find out more information about the Game Config at:
@@ -12,14 +13,21 @@ const config: Phaser.Types.Core.GameConfig = {
   width: 1024,
   height: 768,
   parent: 'game-container',
-  backgroundColor: '#028af8',
+  backgroundColor: '#000000',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { x: 0, y: 0 },
+      debug: false,
+    },
+  },
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
     width: 1024,
     height: 768,
   },
-  scene: [Boot, Preloader, MainMenu, MainGame, GameOver],
+  scene: [Boot, Preloader, MainMenu, MainGame, GameOver, PacmanGame],
 };
 
 const StartGame = (parent: string) => {
